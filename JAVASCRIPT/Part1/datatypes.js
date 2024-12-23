2517898b32b54042a471402730e7038f2fddc603
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-23 23:01:22 +0530

    add the files for datatypes, variables & constants

--- JAVASCRIPT/Part1/datatypes.js
+++ JAVASCRIPT/Part1/datatypes.js
@@ -17,3 +17,32 @@ Datatypes:
     c. Function
 */
 
+var name = "jay govind";
+// console.log(name);
+// var should not be used because it is a global variable.
+// let should be used instead of var.
+
+let score = 100;
+console.log(score);
+console.log(typeof score); // number
+
+let String = "Hello World";
+console.log(String);
+console.log(typeof String); // string
+
+let isTrue = true;
+console.log(isTrue);
+console.log(typeof isTrue); // boolean
+
+// objects & arrays
+let languages = ["Python", "JavaScript", "Java", "C++"];
+console.log(languages);
+console.log(typeof languages); // object
+
+let person = {
+  name: "Govind",
+  age: 20,
+  isStudent: true,
+};
+console.log(person);
+console.log(typeof person); // object
