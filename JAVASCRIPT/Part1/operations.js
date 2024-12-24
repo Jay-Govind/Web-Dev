294a2792fdbfab02b8594c7b7b812b8b41adfd89
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-24 21:33:30 +0530

    add the codes

---
+++ JAVASCRIPT/Part1/operations.js
@@ -0,0 +1,17 @@
+let addtion = 2 + 2;
+console.log(addtion);
+
+let subtraction = 2 - 2;
+console.log(subtraction);
+
+let multiplication = 2 * 2;
+console.log(multiplication);
+
+let division = 2 / 2;
+console.log(division);
+
+let remainder = 2 % 2;
+console.log(remainder);
+
+let exponentiation = 2 ** 2;
+console.log(exponentiation);
