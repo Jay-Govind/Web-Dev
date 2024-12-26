33080b2de661fec2da546e54ee0f2e1766ac6690
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-26 13:22:02 +0530

    revising assignment operators

---
+++ JAVASCRIPT/Part1/assignment.js
@@ -0,0 +1,23 @@
+let score = 10 + 11;
+
+let num1 = 10;
+
+// num1 = num1 + 5
+num1 += 5;
+console.log(num1); // 15
+
+// num1 = num1 - 5
+num1 -= 5;
+console.log(num1); // 10
+
+// num1 = num1 * 5
+num1 *= 5;
+console.log(num1); // 50
+
+// num1 = num1 / 5
+num1 /= 5;
+console.log(num1); // 10
+
+// num1 = num1 % 5
+num1 %= 5;
+console.log(num1); // 0
