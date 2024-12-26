07f8978aee86ad7b73dc96d777f2e2e2c8cac05c
Jay Govind Kumar <govind.iq@gmail.com>
2024-12-26 13:17:47 +0530

    add the codes for operators  revision

---
+++ JAVASCRIPT/Part1/logical.js
@@ -0,0 +1,13 @@
+// && -> and
+// || -> or
+// ! -> not (reverse)
+
+let isLoggedIn = true;
+let isPaidMember = false;
+
+console.log(isLoggedIn && isPaidMember); // false
+
+let isEmailusername = true;
+let isGoogleuser = false;
+
+console.log(isEmailusername || isGoogleuser); // true
--- JAVASCRIPT/Part1/operations.js
+++ JAVASCRIPT/Part1/operations.js
@@ -1,17 +1,33 @@
 let addtion = 2 + 2;
-console.log(addtion);
+// console.log(addtion);
 
 let subtraction = 2 - 2;
-console.log(subtraction);
+// console.log(subtraction);
 
 let multiplication = 2 * 2;
-console.log(multiplication);
+// console.log(multiplication);
 
 let division = 2 / 2;
-console.log(division);
+// console.log(division);
 
 let remainder = 2 % 2;
-console.log(remainder);
+// console.log(remainder);
 
 let exponentiation = 2 ** 2;
-console.log(exponentiation);
+// console.log(exponentiation);
+
+
+// comparison operators
+let num1 = 2;
+let num2 = 2;
+let num3 = 4;
+console.log(num1 == num2); // true
+console.log(num1 != num2); // false
+console.log(num1 > num2); // false
+console.log(num1 < num2); // false
+console.log(num1 >= num2); // true
+console.log(num1 <= num2); // true
+console.log(num1 >= num3); // false
+console.log(num1 <= num3); // true
+
+
